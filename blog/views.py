a3985a2bac70f585c50ef0a3d59ef938ff6e86b0
Nitish <nitish.das@cetindia.net>
2015-05-21 00:05:13 +0530

    created a 2 page website without any design

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from blog.models import Articles
 
-# Create your views here.
+
+
+def index(request):
+    articles_list = Articles.objects.all()
+    context = {'articles': articles_list}
+    #output = '<br/> '.join([p.title for p in articles_list])
+    #return HttpResponse(template.render(context))
+    return render(request, 'blog/index.html', context)
+
+def description(request, article_id):
+    article = get_object_or_404(Articles, pk=article_id)
+    return render(request, 'blog/description.html', {'article': article})
--- blogale/urls.py
+++ blogale/urls.py
@@ -1,10 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'blogale.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('blog.urls')),
+    url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT,
+        }),
 )
